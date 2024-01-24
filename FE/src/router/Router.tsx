89d51370ae05285f6d89846925bd77b8ceeb24d7
felix-alexandria <jesmiah0603@gmail.com>
2024-01-24 16:54:34 +0100

    Created SideBar

--- FE/src/router/Router.tsx
+++ FE/src/router/Router.tsx
@@ -1,31 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Register from "../pages/auth/Register";
-import Signin from "../pages/auth/Signin";
 import Verify from "../pages/auth/Verify";
 import Login from "../pages/auth/Login";
+import AuthLayout from "../Layout/AuthLayout";
 
 export const mainRouter = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children: [
-            {
-                path: "/register",
-                element: <Register/>,
-            },
-            {
-                path: "/Signin",
-                element: <Signin/>,
-            },
-            {
-                path: "/verify",
-                element: <Verify/>,
-            },
-            {
-                path: "/login",
-                element: <Login/>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Layout />,
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "verify",
+        element: <Verify />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
