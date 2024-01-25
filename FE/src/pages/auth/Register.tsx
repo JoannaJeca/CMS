a7e84bddf1790c03475f1b69a499bf2b3e2448d1
JoanJeca <joannajeca936@gmail.com>
2024-01-25 14:58:03 +0100

    Corrected auth mistakex

--- FE/src/pages/auth/Register.tsx
+++ FE/src/pages/auth/Register.tsx
@@ -65,7 +65,7 @@ const Register = () => {
             >
               Already have an account?{" "}
               <NavLink
-                to="/register"
+                to="/auth/register"
                 className="text-blue-600 hover:underline dark:text-blue-500"
               >
                 Login
