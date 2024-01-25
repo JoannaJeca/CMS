2311fdb1eb31eebe03f54b8107b450e869f89a30
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-01-25 15:27:35 +0100

    added header to layout

--- FE/src/Layout/Layout.tsx
+++ FE/src/Layout/Layout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "../pages/SideBar";
-import HomeScreen from "../pages/home/HomeScreen";
+import HeadBlog from "../pages/home/Blog/HeadBlog";
 
 const Layout = () => {
   return (
     <div className="flex justify-center items-center">
       <SideBar />
       <div className="w-[80%]">
+        <HeadBlog />
+
+        <div className="mb-20" />
+
         <Outlet />
       </div>
     </div>
--- FE/src/pages/home/Blog/HeadBlog.tsx
+++ FE/src/pages/home/Blog/HeadBlog.tsx
@@ -3,17 +3,24 @@ import { FaPenClip } from "react-icons/fa6";
 
 const HeadBlog = () => {
   return (
-    <div className="w-full flex justify-center">
-      <div className="w-[70%] h-[60px] bg-white rounded-[40px] top-4 absolute flex justify-between">
+    <div className="w-full  flex justify-center">
+      <div className="w-[70%] border shadow-md h-[60px] bg-white rounded-[40px] top-4 fixed flex justify-between items-center">
         <div className="w-1/2 flex items-center">
           <div className="flex items-center w-full pl-3">
             <FaSearch size={25} />
             <input
               type="text"
-              className="flex-[0.6] border-b border-black outline-none ml-4 py-3"
+              className="flex-[0.6] border-b border-grey-600 outline-none ml-4 py-1"
             />
           </div>
         </div>
+
+        <img
+          src=""
+          className="bg-gray-500 h-[40px] w-[40px] rounded-full  "
+          alt="User Avatar"
+        />
+
         <div className="w-1/2 pr-4 flex justify-end items-center">
           <div className="flex gap-3 items-center cursor-pointer">
             <FaPenClip />
